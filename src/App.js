5c977dd23569c945a811a18f6057375dd90c7c3e
Geunhee Lee <igni950424@gmail.com>
2022-03-02 17:46:10 +0900

    0302

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@
 //import styles from "./App.module.css";
 import {useState, useEffect} from "react";
 
+function Hello(){
+  useEffect(()=>{
+    console.log("created :>");
+    //Cleanup function
+    return () => console.log("destroyed :<");
+  },[]);
+  return <h1>Hello</h1>
+}
+
 function App() {
   const [counter,setValue]=useState(0);
   const [keyword,setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
+
   const onClick = () => setValue((prev)=>prev+1);
   const onChange = (event) => setKeyword(event.target.value);
+  const onClick2 = () => setShowing(prev=>!prev);
+
   //console.log("i run all the time");
 
   useEffect(() => {
@@ -27,6 +40,9 @@ function App() {
       <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."/>
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
+      <br/>
+      <button onClick={onClick2}>{showing ? "Hide" : "Show"}</button>
+      {showing ? <Hello/> : null}
     </div>
   );
 }
